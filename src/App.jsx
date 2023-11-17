4c2f49001137da7d153697314b07867be7892836
TheKorn01 <101316213+TheKorn01@users.noreply.github.com>
2023-11-18 00:09:58 +0700

    updat

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-black rounded-2xl w-[900px]'>
+      <h1 className='text-white '> T-ReactJS</h1>
+      <h1 className='text-white'>XD</h1>
+      </div>
+      <div className="card">
+        <p className='text-[100px]'>TTTTTTTTTT</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
